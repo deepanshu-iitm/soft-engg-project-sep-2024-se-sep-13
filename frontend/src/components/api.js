1d0e9aa2b0005f50894b372a7f05c1692057c772
deepanshu-iitm <156073590+deepanshu-iitm@users.noreply.github.com>
2024-12-11 23:46:58 +0530

    Update api.js

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:5000', 
+    baseURL: 'https://soft-engg-project-sep-2024-se-sep-13-1.onrender.com', 
 });
 
 API.interceptors.request.use((req) => {
